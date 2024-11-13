b74bae1a455b88aadb2ec7f679d3212728de28da
Guna-Pandi <gunapandidurai@gmail.com>
2024-11-14 00:06:04 +0530

    Scrolltop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home, Predictor,Dashboard,ResultPredict } from "../src/components";
 import { cvd } from "../src/Data/data";
+import ScrollTop from "./ScrollTop";
 
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+     <ScrollTop />
       <Routes>
         <Route path="/" element={<Home endpoint={cvd} />} />
         <Route path="/Home/Predictor" element={<Predictor  />} />
